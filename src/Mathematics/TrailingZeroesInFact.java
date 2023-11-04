705a608daaff575405997d463b3aa8c1391b9424
Subhomoy Das <subhomoydas54@gmail.com>
2023-11-04 13:23:38 +0530

    Count the trailing zeroes in factorial of a number.

--- src/Mathematics/TrailingZeroesInFact.java
+++ src/Mathematics/TrailingZeroesInFact.java
@@ -16,7 +16,17 @@ public class TrailingZeroesInFact {
         }
         return count;
     }
+    // optimized solution
+    static int countTrailing(int n){
+        int res = 0;
+        for (int i = 5; i <= n ; i= i*5) {
+            res = res + n/i;
+        }
+        return res;
+    }
     public static void main(String[] args) {
+
         System.out.println(trailingZeros1(5));
+        System.out.println(countTrailing(5));
     }
 }
