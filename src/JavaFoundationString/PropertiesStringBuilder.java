dbcd6f7723209e4fb4ec68970029bcb3b51fae4a
Subhomoy Das <subhomoydas54@gmail.com>
2023-11-07 21:33:17 +0530

    !

---
+++ src/JavaFoundationString/PropertiesStringBuilder.java
@@ -0,0 +1,17 @@
+package JavaFoundationString;
+
+public class PropertiesStringBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("dcba");
+        sb.reverse();
+        System.out.println(sb);
+        sb.append("efg");
+        System.out.println(sb);
+        sb.setCharAt(1,'h');
+        System.out.println(sb);
+        sb.delete(0,2);
+        System.out.println(sb);
+        sb.insert(1,"efg");
+        System.out.println(sb);
+    }
+}
