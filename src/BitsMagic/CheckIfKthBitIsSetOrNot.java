0c93f152e12ddf95ea6398412f20861efa0ee895
Subhomoy Das <subhomoydas54@gmail.com>
2023-12-27 23:43:49 +0530

    Check if the kth bit is set or not!

--- src/BitsMagic/CheckIfKthBitIsSetOrNot.java
+++ src/BitsMagic/CheckIfKthBitIsSetOrNot.java
@@ -14,7 +14,20 @@ public class CheckIfKthBitIsSetOrNot {
             System.out.println("No");
         }
     }
+    // small optimization in naive sol
+    static void isSet2(int n,int k){
+        for (int i = 0; i < k-1; i++) {
+            n = n/2;
+        }
+        if((n&1)!=0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
     public static void main(String[] args) {
         isSet(5,3);
+        isSet2(5,3);
     }
 }
