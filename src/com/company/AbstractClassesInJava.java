875702ba727e555dd7d5204de772a5de2c453d92
Subhomoy Das <subhomoydas54@gmail.com>
2023-12-28 11:33:18 +0530

    Abstraction in Java

--- src/com/company/AbstractClassesInJava.java
+++ src/com/company/AbstractClassesInJava.java
@@ -77,3 +77,14 @@ public class AbstractClassesInJava {
         System.out.println(s2.toString());
     }
 }
+//In Java, abstraction is achieved by interfaces and abstract classes. We can achieve 100% abstraction using interfaces.
+//
+//Abstract classes and Abstract methods :
+//
+//An abstract class is a class that is declared with abstract keyword.
+//An abstract method is a method that is declared without an implementation.
+//An abstract class may or may not have all abstract methods. Some of them can be concrete methods
+//A method defined abstract must always be redefined in the subclass, thus making overriding compulsory OR either make subclass itself abstract.
+//Any class that contains one or more abstract methods must also be declared with abstract keyword.
+//There can be no object of an abstract class. That is, an abstract class can not be directly instantiated with the new operator.
+//An abstract class can have parametrized constructors and the default constructor is always present in an abstract class.
