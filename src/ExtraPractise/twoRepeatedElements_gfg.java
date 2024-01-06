a36f84d06421b1788d0c1874d89be9ea8f60c36a
Subhomoy Das <subhomoydas54@gmail.com>
2024-01-06 13:59:15 +0530

    two repeated number

--- src/ExtraPractise/twoRepeatedElements_gfg.java
+++ src/ExtraPractise/twoRepeatedElements_gfg.java
@@ -1,5 +1,17 @@
 package ExtraPractise;
+//You are given an array of N+2 integer elements. All elements of the array are in the range 1 to N.
+// Also, all elements occur once except two numbers which occur twice. Find the two repeating numbers.
+//
+//Note: Return the numbers in their order of appearing twice. So, if X and Y are the repeating numbers,
+// and X repeats twice before Y repeating twice, then the order should be (X, Y).
 
+//Example 1:
+//Input:
+//N = 4
+//array[] = {1,2,1,3,4,3}
+//Output: 1 3
+//Explanation: In the given array,
+//1 and 3 are repeated two times.
 public class twoRepeatedElements_gfg {
     static int[] twoRepeated(int [] arr , int N){
         int ans[] = new int[2];
