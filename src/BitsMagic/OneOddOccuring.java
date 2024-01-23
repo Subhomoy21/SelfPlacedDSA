95472f6b5c0138f859b188bcc1d84bbceab77dd5
Subhomoy Das <subhomoydas54@gmail.com>
2024-01-23 12:42:54 +0530

    one odd occurring

--- src/BitsMagic/OneOddOccuring.java
+++ src/BitsMagic/OneOddOccuring.java
@@ -18,8 +18,16 @@ public class OneOddOccuring {
         return 0;
     }
     //efficient solution
+    static int findOdd2(int [] arr){
+        int res = arr[0];
+        for (int i = 1; i < arr.length ; i++) {
+            res  = res^arr[i];
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int []arr = {4,4,7,4,8,7,7,7,8};
         System.out.println(findOdd(arr));
+        System.out.println(findOdd2(arr));
     }
 }
