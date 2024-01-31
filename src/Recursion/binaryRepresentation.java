b14955f3228ddca4699475011c3395de553f062d
Subhomoy Das <subhomoydas54@gmail.com>
2024-01-31 09:30:10 +0530

    binary representation using recursion..

---
+++ src/Recursion/binaryRepresentation.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class binaryRepresentation {
+    static void fun(int n){
+        if(n==0){
+            return;
+        }
+        fun(n/2);
+        System.out.print(n%2);
+    }
+    public static void main(String[] args) {
+        fun(13); // binary mein hona chaiye 1101
+    }
+}
