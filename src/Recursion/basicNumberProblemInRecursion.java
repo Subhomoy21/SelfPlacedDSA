3960277cf344b65af68687b8ae1d345b6bee3439
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-04 11:51:07 +0530

    Basic number problem using recursion

--- src/Recursion/basicNumberProblemInRecursion.java
+++ src/Recursion/basicNumberProblemInRecursion.java
@@ -7,8 +7,14 @@ public class basicNumberProblemInRecursion {
         }
         return n * fact(n-1);
     }
-
+    static int nthFibonacci(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return nthFibonacci(n-1) + nthFibonacci(n-2);
+    }
     public static void main(String[] args) {
         System.out.println(fact(5));
+        System.out.println(nthFibonacci(3));
     }
 }
