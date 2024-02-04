aedefa64a033834552dfcb3a6b5d39400ffdfe77
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-04 11:52:57 +0530

    Basic number problem using recursion

--- src/Recursion/basicNumberProblemInRecursion.java
+++ src/Recursion/basicNumberProblemInRecursion.java
@@ -13,8 +13,15 @@ public class basicNumberProblemInRecursion {
         }
         return nthFibonacci(n-1) + nthFibonacci(n-2);
     }
+    static int sum(int n){ // n = 4 {1+2+3+4 = 10}
+        if(n==0){
+            return 0;
+        }
+        return n + sum(n-1);
+    }
     public static void main(String[] args) {
         System.out.println(fact(5));
         System.out.println(nthFibonacci(3));
+        System.out.println(sum(4));
     }
 }
