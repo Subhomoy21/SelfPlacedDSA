d0fbed749de256d282f93d21482c6d0f1a6f28a7
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-08 21:17:46 +0530

    largest element in array

--- src/Arrays/largestElementInArray.java
+++ src/Arrays/largestElementInArray.java
@@ -29,19 +29,29 @@ public class largestElementInArray {
         return max;
     }
     // optimised - (2)
-    static int large2(int [] arr){
+    static int[] large2(int [] arr){
+        int [] storeIndAndEle = new int[2];
         int max = arr[0];
+        int res = 0;
         for (int i = 1; i < arr.length ; i++) {
             if(arr[i] > max){
+                res = i;
                 max = arr[i];
             }
         }
-        return max;
+        for (int i = 0; i < storeIndAndEle.length; i++) {
+            storeIndAndEle[0] = res;
+            storeIndAndEle[1] = max;
+        }
+        return storeIndAndEle;
     }
     public static void main(String[] args) {
         int [] arr = {10,5,0,12};
         System.out.println(largest(arr));
         System.out.println(large(arr));
-        System.out.println(large2(arr));
+        int[] ans = large2(arr);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
     }
 }
