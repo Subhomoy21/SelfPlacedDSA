d47dd485a3323421f66eeeea3e9aa4c4932320e7
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-11 12:01:53 +0530

    seclargest element in array

--- src/Arrays/secLargestInArray.java
+++ src/Arrays/secLargestInArray.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+// we are finding the index !
+
 public class secLargestInArray {
     // linear naive solution
     static int getLargest(int arr[]){
@@ -28,10 +30,27 @@ public class secLargestInArray {
         }
         return res;
     }
-    // 2nd one
+    // 2nd one(using one traversal)
+    static int secLargest2(int [] arr){
+        int res = -1;
+        int largest = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > arr[largest]){
+                res = largest;
+                largest = i;
+            }
+            else if(arr[i] != arr[largest]){
+                if((res == -1) || (arr[i] > arr[res])){
+                    res = i;
+                }
+            }
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         int arr[] = {5,20,12,20,10};
         System.out.println(secLargest(arr));
+        System.out.println(secLargest2(arr));
     }
 }
