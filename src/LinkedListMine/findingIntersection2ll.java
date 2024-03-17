88401f09ec88b2af8b2e1894247be925f40fb92d
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-17 19:59:25 +0530

    !!!!!!!!!!!!!!!!!!!!!!!

--- src/LinkedListMine/findingIntersection2ll.java
+++ src/LinkedListMine/findingIntersection2ll.java
@@ -16,26 +16,61 @@ public class findingIntersection2ll {
         }
         System.out.println();
     }
+     static Node getIntersectionNode(Node headA, Node headB) {
+        Node tempA = headA;
+        Node tempB = headB;
+        int lenA= 0;
+        while(tempA != null){
+            tempA = tempA.next;
+            lenA++;
+        }
+        int lenB = 0;
+        while(tempB != null){
+            tempB = tempB.next;
+            lenB++;
+        }
+        tempA = headA;
+        tempB = headB;
+        // check karo konsa list barra hai
+        // uska baad slow fast pointer lago
+        if(lenB>lenA){
+            int size = lenB - lenA;
+            for(int i = 1;i<=size;i++){
+                tempB = tempB.next;
+            }
+        }
+        else{
+            int size = lenA - lenB;
+            for(int i = 1;i<=size;i++){
+                tempA = tempA.next;
+            }
+        }
+        while(tempA != tempB){
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+        return tempA;
+    }
     public static void main(String[] args) {
-        Node a = new Node(5);
-        Node b = new Node(3);
-        Node c = new Node(7);
-        Node d = new Node(4);
-        Node e = new Node(2);
-        Node f = new Node(8);
-        a.next = b; // connection hua bhai
-        b.next = c;
-        c.next = d;
-        d.next  = e;
-        e.next = f;
-        Node a1 = new Node(5);
-        Node b1 = new Node(3);
-        Node c1 = new Node(7);
-        Node d1 = new Node(4);
-        a1.next = b1; // connection hua bhai
-        b1.next = c1;
-        c1.next = d1;
-
+//        Node a = new Node(5);
+//        Node b = new Node(3);
+//        Node c = new Node(7);
+//        Node d = new Node(4);
+//        Node e = new Node(2);
+//        Node f = new Node(8);
+//        a.next = b; // connection hua bhai
+//        b.next = c;
+//        c.next = d;
+//        d.next  = e;
+//        e.next = f;
+//        Node a1 = new Node(5);
+//        Node b1 = new Node(3);
+//        Node c1 = new Node(7);
+//        Node d1 = new Node(4);
+//        a1.next = b1; // connection hua bhai
+//        b1.next = c1;
+//        c1.next = d1;
+//        System.out.println(getIntersectionNode(a,a1));
 
     }
 }
