90a66846fdb85c2f5fb4256ed6a2a734d4f44838
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-26 20:07:25 +0530

    remove Duplicates From SortedArray

--- src/Arrays/removeDuplicatesFromSortedArray.java
+++ src/Arrays/removeDuplicatesFromSortedArray.java
@@ -18,6 +18,17 @@ public class removeDuplicatesFromSortedArray {
         }
         return res;
     }
+    // efficient solution
+    static int rem(int [] arr){
+        int res = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[res -1]){
+                arr[res] = arr[i];
+                res++;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int arr[] = {10, 20, 20, 30, 30, 30};
         int n  = arr.length;
@@ -33,6 +44,8 @@ public class removeDuplicatesFromSortedArray {
         {
             System.out.print(arr[i]+" ");
         }
-
+        System.out.println();
+        System.out.println(remDuplicates(arr));
+        System.out.println(rem(arr));
     }
 }
