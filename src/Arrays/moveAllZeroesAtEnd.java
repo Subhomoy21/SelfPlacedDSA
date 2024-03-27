55d4d4a439932445150bda1f2299fe44f1ad8ea9
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-27 19:57:18 +0530

    move All zeroes to end.......

---
+++ src/Arrays/moveAllZeroesAtEnd.java
@@ -0,0 +1,31 @@
+package Arrays;
+import java.util.*;
+public class moveAllZeroesAtEnd {
+    static void swap(int [] arr,int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static void moveZeros(int [] arr){
+        // naive solution
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == 0){
+                for (int j = i+1; j < n ; j++) {
+                    if(arr[j] !=  0){
+                        swap(arr,arr[i],arr[j]);
+                    }
+                }
+            }
+        }
+    }
+    // efficient solution
+
+    public static void main(String[] args) {
+        int arr[] = {5,2,0,2,0,0,4,5};
+        moveZeros(arr);
+        System.out.println(Arrays.toString(arr));
+       // swap(4,5);
+
+    }
+}
