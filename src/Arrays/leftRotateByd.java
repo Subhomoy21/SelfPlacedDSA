a42e11abb5d76e0e28d3cd3bcbc6058bafd4a953
Subhomoy Das <subhomoydas54@gmail.com>
2024-04-01 09:04:24 +0530

    left rotate an array by d values

--- src/Arrays/leftRotateByd.java
+++ src/Arrays/leftRotateByd.java
@@ -22,10 +22,26 @@ public class leftRotateByd {
             leftRotateOne(arr);
         }
     }
+    // better solution
+    static void leftRotateBetter (int [] arr , int d){
+        int n = arr.length;
+        int [] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+        for (int i = d; i < n; i++) {
+            arr[i-d] = arr[i];
+        }
+        for (int i = 0; i < d; i++) {
+            arr[n-d+i] = temp[i];
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
         displayArr(arr);
-        leftRotateByd(arr,2);
+//        leftRotateByd(arr,2);
+//        displayArr(arr);
+        leftRotateBetter(arr,2);
         displayArr(arr);
     }
 }
