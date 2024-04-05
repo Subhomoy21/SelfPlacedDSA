91e42fb38a4b003048c2a2568203092976266fdb
Subhomoy Das <subhomoydas54@gmail.com>
2024-04-05 12:43:25 +0530

    maximum difference in an array!

--- src/Arrays/maximumDifference.java
+++ src/Arrays/maximumDifference.java
@@ -12,8 +12,23 @@ public class maximumDifference {
         }
         return res;
     }
+    // from deeskha di notes
+    static int maxDifference(int[] nums) {
+        //naive solution
+        int diff = 0;
+        int  n = nums.length;
+        for(int i = 0; i<n-1;i++){
+            for(int j = i+1 ;j<n ;j++){
+               if(nums[j] - nums[i] >  diff){
+                   diff = nums[j] - nums[i];
+               }
+            }
+        }
+        return diff;
+    }
     public static void main(String[] args) {
         int arr[] = {2, 3, 10, 6, 4, 8, 1};
         System.out.println(maximumDifference(arr));
+        System.out.println(maxDifference(arr));
     }
 }
