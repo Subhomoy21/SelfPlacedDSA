6ee37dea4b361806878d18f428670dfcd6345d53
Subhomoy Das <subhomoydas54@gmail.com>
2024-04-06 20:49:53 +0530

    maximum difference

--- src/Arrays/maximumDifference.java
+++ src/Arrays/maximumDifference.java
@@ -27,10 +27,20 @@ public class maximumDifference {
         return diff;
     }
     // efficient solution!
-
+    static int maxDiffEff(int [] arr){
+        int n = arr.length;
+        int res = arr[1] - arr[0];
+        int miVal = arr[0];
+        for (int i = 1; i <n ; i++) {
+            res = Math.max(res,arr[i]-miVal);
+            miVal = Math.min(arr[i] , miVal);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int arr[] = {2, 3, 10, 6, 4, 8, 1};
         System.out.println(maximumDifference(arr));
         System.out.println(maxDifference(arr));
+        System.out.println(maxDiffEff(arr));
     }
 }
