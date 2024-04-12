7f512d0cf5ef6d2b55ec7d56903b6a965f46f8d3
Subhomoy Das <subhomoydas54@gmail.com>
2024-04-12 13:18:18 +0530

    best time to buy and sell stocks

--- src/Arrays/StockBuyAndSell.java
+++ src/Arrays/StockBuyAndSell.java
@@ -17,8 +17,19 @@ public class StockBuyAndSell {
         }
         return profit;
     }
+    // efficient solution
+    static int maxProfits(int[] prices) {
+        int profit = 0;
+        for(int i = 1;i<prices.length;i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
     public static void main(String[] args) {
         int [] prices = {100, 180, 260, 310, 40, 535, 695};
         System.out.println(maxProfit(prices,0,prices.length-1));
+        System.out.println(maxProfits(prices));
     }
 }
