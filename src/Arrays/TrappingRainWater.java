bd0c5fc2b923632ed08ef12ae4a895887f1f8676
Subhomoy Das <subhomoydas54@gmail.com>
2024-04-19 19:43:50 +0530

    trapping water important question

--- src/Arrays/TrappingRainWater.java
+++ src/Arrays/TrappingRainWater.java
@@ -18,8 +18,30 @@ public class TrappingRainWater {
         }
         return res;
     }
+    // another process
+    static int getWater2(int [] arr){
+        int n = arr.length;
+        int res = 0;
+        for (int i = 1 ; i < n-1 ; i++) {
+            int lmax= arr[i];
+            for (int j = 0; j < i; j++) {
+                if(arr[j] > lmax){
+                    lmax = arr[j];
+                }
+            }
+            int rmax = arr[i];
+            for (int j = i+1; j < n ; j++) {
+                if(arr[j] > rmax){
+                    rmax = arr[j];
+                }
+            }
+            res = res + Math.min(lmax,rmax) - arr[i];
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int arr[] = {3, 0, 1, 2, 5};
         System.out.println(getWater(arr));
+        System.out.println(getWater2(arr));
     }
 }
