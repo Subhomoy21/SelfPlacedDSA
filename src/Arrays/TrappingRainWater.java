821fb2deb654da202f25f071b32141a118c56dfa
Subhomoy Das <subhomoydas54@gmail.com>
2024-05-09 13:33:54 +0530

    trapping water important question

--- src/Arrays/TrappingRainWater.java
+++ src/Arrays/TrappingRainWater.java
@@ -39,9 +39,29 @@ public class TrappingRainWater {
         }
         return res;
     }
+    // efficient solution
+    static int getWatereff(int [] arr){
+        int n = arr.length;
+        int res = 0;
+        int[] lmax = new int[n];
+        int [] rmax = new int[n];
+        lmax[0] = arr[0];
+        for (int i = 1; i < n ; i++) {
+            lmax[i] = Math.max(lmax[i-1],arr[i]); // left side mein konsa block barra!
+        }
+        rmax[n-1] = arr[n-1];
+        for (int i = n-2; i >= 0 ; i--) {
+            rmax[i] = Math.max(rmax[i+1] , arr[i]);
+        }
+        for (int i = 1; i < n-1 ; i++) {
+            res = res + Math.min(lmax[i],rmax[i]) - arr[i];
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int arr[] = {3, 0, 1, 2, 5};
         System.out.println(getWater(arr));
         System.out.println(getWater2(arr));
+        System.out.println(getWatereff(arr));
     }
 }
