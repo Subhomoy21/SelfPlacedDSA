c5d995228887456618f9dc939e93786c2266ecc3
Subhomoy Das <subhomoydas54@gmail.com>
2024-05-26 13:11:04 +0530

    Maximum SubArray Sum in the array.........

--- src/Arrays/maximumSumSubarray.java
+++ src/Arrays/maximumSumSubarray.java
@@ -13,8 +13,21 @@ public class maximumSumSubarray {
         }
         return maximum;
     }
+    // efficient solution
+     // O(n)
+    static int maxSum(int[]arr){
+        int n = arr.length;
+        int res = arr[0];
+        int maxEnd = arr[0];
+        for (int i = 1; i < n ; i++) {
+            maxEnd = Math.max(maxEnd + arr[i] , arr[i]);
+            res = Math.max(res,maxEnd);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int [] arr = {5,8,3};
         System.out.println(maxSumSub(arr));
+        System.out.println(maxSum(arr));
     }
 }
